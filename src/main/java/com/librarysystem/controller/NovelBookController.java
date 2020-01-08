3fb23a732b164b954927964d76b37292ad9e5cf7
ZHANZHOU TA <57724490+tazhanzhou@users.noreply.github.com>
2020-01-08 13:26:56 -0500

    Update NovelBookController.java

--- src/main/java/com/librarysystem/controller/NovelBookController.java
+++ src/main/java/com/librarysystem/controller/NovelBookController.java
@@ -1,15 +1,30 @@
-package com.librarysystem.controller;
+package controller;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.librarysystem.service.BookService;
+import model.Book;
+import service.BookService;
 
 @Controller
 @RequestMapping("/novelbooks")
-public class NovelBookController {
+public class NovelBooksController {
 	@Autowired
 	BookService service;
+	
+	@RequestMapping(value = "/getnovelbooks", method = RequestMethod.GET)
+	public ModelAndView getNovelBookList() {
+		ModelAndView modelView = new ModelAndView("novel-books-list");
+
+		List<Book> books = service.getNovelBooks();
+		modelView.addObject("bookList", books);
+		
+		return modelView;
+	}
 
 }
